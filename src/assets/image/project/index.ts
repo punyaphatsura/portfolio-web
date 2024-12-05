f3ebdef8e8f3d56b193e8091f791263891760630
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2024-12-06 04:08:19 +0700

    feat: :bento: Add asset

---
+++ src/assets/image/project/index.ts
@@ -0,0 +1,9 @@
+import Crafty1 from './crafty-1.png';
+import Crafty2 from './crafty-2.png';
+import Crafty3 from './crafty-3.png';
+import MassengerWebChat from './massager-webChat.png';
+import PaperRef1 from './paper-ref-1.png';
+import PaperRef2 from './paper-ref-2.png';
+import PersonalWeb from './personal-web.png';
+
+export { Crafty1, Crafty2, Crafty3, MassengerWebChat, PersonalWeb, PaperRef1, PaperRef2 };
---
+++ src/assets/tech/index.ts
@@ -0,0 +1,33 @@
+import Asp from './asp.svg';
+import Css from './css3-colored.svg';
+import Elysia from './elysia.svg';
+import Figma from './figma-colored.svg';
+import Firebase from './firebase.svg';
+import Html from './html5-colored.svg';
+import Javascript from './javascript-colored.svg';
+import MongoDB from './mongodb-colored.svg';
+import Nextjs from './nextjs.svg';
+import Python from './python-colored.svg';
+import ReactIcon from './react-colored.svg';
+import ReactNative from './react-native.svg';
+import Tailwind from './tailwindcss-colored.svg';
+import Typescript from './typescript-colored.svg';
+import VueJS from './vuejs-colored.svg';
+
+export {
+  Elysia,
+  Css,
+  Html,
+  Typescript,
+  MongoDB,
+  Nextjs,
+  ReactNative,
+  ReactIcon,
+  Tailwind,
+  Javascript,
+  Figma,
+  VueJS,
+  Asp,
+  Python,
+  Firebase,
+};
