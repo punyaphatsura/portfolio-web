88965107a5b2a9b8a153582ce7c209b795527901
punyaphatsura <punyaphat.sura@gmail.com>
2025-12-08 14:01:28 +0700

    fix: hover effect not smooth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,29 @@ import Contact from '@/components/Contact';
 import Intro from '@/components/Intro';
 import Project from '@/components/Project';
 import Experience from '@components/Experience';
-import { motion, useScroll } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+} from 'framer-motion';
+import { useEffect } from 'react';
 
 export default function Page() {
-  const [mousePosition, setMousePosition] = useState<{ x: number; y: number }>({
-    x: 0,
-    y: 0,
-  });
-
   const { scrollY } = useScroll();
+
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const springConfig = { damping: 25, stiffness: 200 };
+  const springX = useSpring(mouseX, springConfig);
+  const springY = useSpring(mouseY, springConfig);
+
+  const absoluteY = useTransform([springY, scrollY], (latest: any[]) => latest[0] + latest[1]);
+
+  const background = useMotionTemplate`radial-gradient(circle at ${springX}px ${absoluteY}px, #0B122A 0%, #000 25%)`;
 
   useEffect(() => {
     window.scroll({ behavior: 'smooth', top: 0 });
@@ -22,34 +35,21 @@ export default function Page() {
 
   useEffect(() => {
     const onMouseMove = (event: MouseEvent) => {
-      setMousePosition({ x: event.clientX, y: scrollY.get() + event.clientY });
+      mouseX.set(event.clientX);
+      mouseY.set(event.clientY);
     };
 
     window.addEventListener('mousemove', onMouseMove);
 
-    // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener('mousemove', onMouseMove);
     };
-  }, [scrollY]); // Depend on scrollY to update the effect on scroll
-
-  useEffect(() => {
-    const onScroll = (event: WheelEvent) => {
-      setMousePosition({ x: event.clientX, y: scrollY.get() + event.clientY });
-    };
-    window.addEventListener('wheel', onScroll);
-    return () => {
-      window.removeEventListener('wheel', onScroll);
-    };
-  }, [scrollY]);
+  }, [mouseX, mouseY]);
 
   return (
     <motion.div
       initial={{ background: '#080e21' }}
-      animate={{
-        background: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, #0B122A 0%, #000 25%)`,
-      }}
-      transition={{ ease: 'linear', duration: 0.1 }}
+      style={{ background }}
       className="max-w-screen h-full bg-black">
       <div className="h-[1500px] w-full">
         <Intro />
